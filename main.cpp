3c2c41f829ddbbf08415632bf8b8c0f8e0930064
Nol111 <113212776+Nol111@users.noreply.github.com>
2022-11-23 20:12:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void radixsort(vector<string>&a, int m, int k, int i){
+    vector<string> result(a.size(), "");
+    vector<int> count(26, 0);
+    for(int j = 0; j < a.size(); j++){
+        count[a[j][i] - 'a']++;
+    }
+    int temp = 0;
+    int p = 0;
+    for(int j = 0; j < count.size(); j++){
+        temp = count[j];
+        count[j] = p;
+        p = p + temp;
+    }
+    for(int j = 0; j < a.size(); j++){
+        result[count[a[j][i] - 'a']] = a[j];
+        count[a[j][i] - 'a']++;
+     }
+    a = result;
+}
+int main(){
+    int x, y, z;
+    cin >> x >> y >> z;
+    vector<string> a(x, "");
+    for (int i = 0; i < x; i++) {
+        cin >> a[i];
+    }
+    for (int i = y - 1; i > y - z - 1; i--){
+        radixsort(a, y, z, i);
+    }
+    for (int i = 0; i < x; i++) {
+        cout << a[i]<<'\n';
+    };
+}
+
+
+
